6a7d1547f74383bba81454ded24aacff05f96122
Monika <pulcovamon@gmail.com>
2023-04-10 13:15:00 +0200

    data types

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+Names of classes start with uppercase,
+names of methods start with lowercase. 
+
+Java basic structure:
+*/
+
+public class Main {     //public/private - acces modifier
+    public static void main(String[] args) {       // main method is always static
+        int myAge = 30;
+        int herAge = myAge;                     // cammel case noteation
+        System.out.println(herAge);      // print line method      
+    }
+}
